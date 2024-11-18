7a137888d14bd88395f06df271d829527d93943e
Aadil <md.aadil.shafi@gmail.com>
2024-11-18 22:40:55 +0530

    basic add funciton for commas handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
 const App = () => {
+
+  function add(numbers:string):number {
+    if(!numbers) return 0;
+    //for commas handling
+    let temp = numbers.split(',').reduce((item, acc)=> Number(item) + Number(acc), 0);
+    return temp;
+  }
+  console.log(add('1, 1, 2'))
+
   return (
     <div>App</div>
   )
